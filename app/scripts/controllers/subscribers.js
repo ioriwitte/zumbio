86be82eecd397e3fdd1b5bfed3a4b2542304dccf
ioriwitte <iori.witte@runbox.com>
2017-04-20 22:27:31 +0200

    Update subscribers.js

--- app/scripts/controllers/subscribers.js
+++ app/scripts/controllers/subscribers.js
@@ -10,7 +10,7 @@
 angular.module('hlApp')
 
 .controller('SubscribersCtrl', function ($scope,$http,$modal) {
-$http.get('http://localhost:9000/subscribers.json')
+$http.get('https://localhost:9000/subscribers.json')
 .then(function (response){
 $scope.subscribers = response.data;
 });
