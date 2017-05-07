87cd1c3685eed15c543bc48e81ca6380e721e304
ioriwitte <iori.witte@runbox.com>
2017-05-08 01:46:41 +0200

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,43 +1,33 @@
-'use strict';
-
-/**
- * @ngdoc overview
- * @name hl2304App
- * @description
- * # hl2304App
- *
- * Main module of the application.
- */
-angular
-  .module('hl2304App', ['ngRoute','ui.bootstrap','ui.grid'])
-
+angular.module('hlApp', ['ngRoute','ui.bootstrap','ui.grid','ngAnimate'])
 
 .config(['$locationProvider', function($locationProvider) {
   $locationProvider.hashPrefix('');
 }])
+
+.config(function ($routeProvider) {
+
+  $routeProvider
+ .when('/', {
+   templateUrl: 'views/main.html',
+   controller: 'MainCtrl'
+   })
+  .when('/subscribers', {
+    templateUrl: 'views/subscribers.html',
+    controller: 'SubscribersCtrl',
+    controllerAs: 'subscribers'
+  })
+  .when('/team', {
+    templateUrl: 'views/team.html',
+    controller: 'TeamCtrl',
+    controllerAs: 'team'
+  })
+  .when('/product', {
+    templateUrl: 'views/product.html',
+    controller: 'ProductCtrl',
+    controllerAs: 'product'
+  })
+  .otherwise({
+redirectTo: '/'
+});
   
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-      })
-      .when('/product', {
-        templateUrl: 'views/product.html',
-        controller: 'ProductCtrl',
-        controllerAs: 'product',
-      })
-      .when('/team', {
-        templateUrl: 'views/team.html',
-        controller: 'TeamCtrl',
-        controllerAs: 'team'
-      })
-      .when('/registration', {
-        templateUrl: 'views/registration.html',
-        controller: 'RegistrationCtrl',
-        controllerAs: 'registration'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+});
