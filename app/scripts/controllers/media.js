107c5daa27464130d3615ce19f3091526a5b84a5
ioriwitte <iori.witte@runbox.com>
2017-05-26 23:37:46 +0200

    Create media.js

---
+++ app/scripts/controllers/media.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hl2304App.controller:MediaCtrl
+ * @description
+ * # MediaCtrl
+ * Controller of the hl2304App
+ */
+
+
+
+
+angular.module('hl2304App')
+
+.controller('MediaCtrl', function ($scope, youtubeEmbedUtils) {
+  // 'VvTvtIeXJ1I'
+  //console.log(youtubeEmbedUtils.getIdFromURL('https://www.youtube.com/watch?v=suG-c_i2fBQ'));
+//}
+  
+
+  $scope.playerVars = {
+    controls: 0,
+    autoplay: 1
+ };
+
+
+  $scope.anotherGoodOne = 'https://www.youtube.com/watch?v=suG-c_i2fBQ';
+});
+
